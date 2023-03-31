cedd107ab3813be1b58c5b3c60b6749e41058840
rafabg26 <rafabenaventgarcia29@gmail.com>
2023-03-31 12:45:45 +0200

    arreglado el problema del fondo

--- Neighbour Brawl/Assets/Scripts/ParallaxScrolling.cs
+++ Neighbour Brawl/Assets/Scripts/ParallaxScrolling.cs
@@ -7,14 +7,20 @@ public class ParallaxScrolling : MonoBehaviour
     private float width, startPosition;
     private Transform cam;
     public float parallaxFraction;
+    private SpriteRenderer spriteRenderer;
+    private float offset, moved;
+
     void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         startPosition = transform.position.x;
-        width = GetComponent<SpriteRenderer>().bounds.size.x;
+        width = spriteRenderer.bounds.size.x;
         cam = Camera.main.transform;
     }
-    void LateUpdate() {
-        float offset = (cam.position.x * parallaxFraction);
-        float moved = cam.position.x - offset;
+    void Update() {
+        offset = (cam.position.x * parallaxFraction);
+        moved = cam.position.x - offset;
+    }
+    void FixedUpdate() {
         if (moved > startPosition + width) startPosition += width;
         else if (moved < startPosition - width) startPosition -= width;
         transform.position = new Vector3(startPosition + offset,
