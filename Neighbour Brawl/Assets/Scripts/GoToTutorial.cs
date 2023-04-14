832bd7ff15146be9eb824f73e40911193ca0ceff
rafabg26 <rafabenaventgarcia29@gmail.com>
2023-04-14 16:22:31 +0200

    Menu principal

---
+++ Neighbour Brawl/Assets/Scripts/GoToTutorial.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToTutorial : MonoBehaviour
+{
+    public void CargarLevel0()
+    {
+        SceneManager.LoadScene("Level0");
+    }
+}
+
