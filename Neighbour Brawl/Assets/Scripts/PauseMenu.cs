7f917a118c625b608954383995437416b792fc3a
PabloPS21 <pabloserrano.est21@gmail.com>
2023-04-15 13:16:06 +0200

    Creación menú de pausa

---
+++ Neighbour Brawl/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject PauseCanvas;
+    public static bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(isPaused){
+                ResumeGame();
+            } else {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame(){
+        PauseCanvas.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame(){
+        PauseCanvas.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void GoToMainMenu(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
