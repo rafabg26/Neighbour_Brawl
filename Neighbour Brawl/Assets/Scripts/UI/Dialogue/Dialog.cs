0f6c22e1803eaaa69ca3793a8ba3977fcdf3ec3b
PabloPS21 <pabloserrano.est21@gmail.com>
2023-04-20 17:22:26 +0200

    Corrección omitir texto

--- Neighbour Brawl/Assets/Scripts/UI/Dialogue/Dialog.cs
+++ Neighbour Brawl/Assets/Scripts/UI/Dialogue/Dialog.cs
@@ -67,7 +67,8 @@ public class Dialog : MonoBehaviour
 
     public void OmitirText(){
        Destroy(dialogueObject); 
-        ActivarMovimiento();
+       timer.SetActiveTimer(true);
+       ActivarMovimiento();
     }
 
     public void ActivarMovimiento(){
