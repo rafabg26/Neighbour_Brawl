34c6f1e1be9aeb40e400ef6ff25d6c4e85307b28
piliv4 <79758821+piliv4@users.noreply.github.com>
2023-04-23 15:09:49 +0200

    Correccion animacion daño MC

--- Neighbour Brawl/Assets/Scripts/Characters/PlayerDamage.cs
+++ Neighbour Brawl/Assets/Scripts/Characters/PlayerDamage.cs
@@ -6,6 +6,7 @@ public class PlayerDamage : MonoBehaviour
 {
     
     public HealthBar healthBar;
+    private Rigidbody2D _body;
     public int maxHealth = 100;
     private int currentHealth;
     private Animator _anim;
@@ -14,12 +15,15 @@ public class PlayerDamage : MonoBehaviour
     void Start()
     {
         _anim = GetComponent<Animator>();
+        _body = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
 
     }
     public void TakeDamage(int damage){
-        _anim.Play("Damaged");
+        _body.velocity = new Vector2(0,0);;
+        _anim.ResetTrigger("Punch");
+        _anim.SetTrigger("Damaged");
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
         if(currentHealth <= 0){
