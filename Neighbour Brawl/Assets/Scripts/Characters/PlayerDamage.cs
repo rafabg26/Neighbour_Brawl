733fb7b846572f84a4a2ea317d1da94bac35d618
PabloPS21 <pabloserrano.est21@gmail.com>
2023-05-02 18:56:15 +0200

    Añadir knockback de protagonista

--- Neighbour Brawl/Assets/Scripts/Characters/PlayerDamage.cs
+++ Neighbour Brawl/Assets/Scripts/Characters/PlayerDamage.cs
@@ -11,6 +11,8 @@ public class PlayerDamage : MonoBehaviour
     private int currentHealth;
     private Animator _anim;
     private Timer label;
+
+    public bool _canBeHit;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +30,42 @@ public class PlayerDamage : MonoBehaviour
         healthBar.SetHealth(currentHealth);
         if(currentHealth <= 0){
             GameController.Instance.EndLevel("YOU LOSE");
+        }
+        StopCoroutine("HitCoroutine"); 
+        StartCoroutine("HitCoroutine");
+    }
+
+    private IEnumerator HitCoroutine() {
+        _canBeHit = false;  // Desactiva la capacidad de ser golpeado
+        _body.velocity = Vector2.zero;  // Detiene cualquier movimiento del personaje
+        _body.isKinematic = true;  // Desactiva temporalmente el Rigidbody
+        float originalGravityScale = _body.gravityScale;  // Guarda el valor original de la gravedad
+        _body.gravityScale = 0f;  // Desactiva temporalmente la gravedad
+
+        float originalX = transform.position.x;
+        float targetX = transform.position.x - 0.5f;
+        float elapsedTime = 0f;
+        float moveTime = 0.10f;  // Tiempo total de movimiento hacia atrás
+        float speed = (targetX - originalX) / moveTime;  // Velocidad de movimiento hacia atrás
+
+        while (elapsedTime < moveTime) {
+            float newX = transform.position.x - (speed * Time.deltaTime * -1.5f);
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        _body.isKinematic = false;  // Reactiva el Rigidbody
+        _body.gravityScale = originalGravityScale;  // Reactiva la gravedad
+        _canBeHit = true;  // Activa la capacidad de ser golpeado nuevamente
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            // Código que se ejecutará cuando haya una colisión con el personaje
+            TakeDamage(10);
         }
     }
 }
